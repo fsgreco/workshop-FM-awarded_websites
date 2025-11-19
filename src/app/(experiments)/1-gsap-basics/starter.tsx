86c87feafc295bf9869677b856fc2d2ebfb9350b
Santiago <fsgreco@fastmail.com>
2025-11-19 12:47:16 +0100

    Part 1 - Gsap basics and CSS animations

--- src/app/(experiments)/1-gsap-basics/starter.tsx
+++ src/app/(experiments)/1-gsap-basics/starter.tsx
@@ -1,15 +1,52 @@
 "use client";
 
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+import React from "react";
+
+
+const animatedParams: gsap.TweenVars = {
+	y: -100,
+	x: 100,
+	opacity: 1,
+	stagger: 0.03,
+	duration: 1.3,
+	ease: "circ.out",
+	onUpdate: () => {console.log('updating', Date.now())}
+}
+
+
+// css selector, animatedParams, reference
+interface AnimationHookOb { selector: string; animatedParams: gsap.TweenVars; reference: React.RefObject<HTMLDivElement | null> }
+const useGSAPAnimation = ({selector,animatedParams,reference}: AnimationHookOb) => {
+	const animationCallback = () => gsap.to(selector, animatedParams)
+	useGSAP(animationCallback, { scope: reference})
+}
+
 export default function Page() {
-  return (
-    <div className="bg-blue-300 text-black">
-      <div className="flex h-screen items-end justify-left overflow-hidden">
-        <h1 className="title font-black text-[min(20rem,30vw)] leading-none pb-[0.1em] text-left">
-          GSAP
-          <br />
-          tweens
-        </h1>
-      </div>
-    </div>
-  );
+	const element = React.useRef<HTMLDivElement>(null)
+
+	// React.useEffect(() => {
+	// 	const ctx = gsap.context( () => gsap.to(".title", animatedParams), element )
+	// 	return () => ctx.revert()
+	// }, [])
+
+	// They created this useGSAP that handle the context and the useEffect logic for you
+	// useGSAP(() => gsap.to(".title", animatedParams), { scope: element })
+
+	// I abstract that even more
+	useGSAPAnimation({ animatedParams, reference: element, selector: ".title" });
+
+	return (
+		<div className="bg-blue-300 text-black">
+			<div className="flex h-screen items-end justify-left overflow-hidden" ref={element}>
+				<h1 className="title font-black text-[min(20rem,30vw)] leading-none pb-[0.1em] text-left">
+					GSAP
+					<br />
+					tweens
+				</h1>
+			</div>
+		</div>
+	);
 }
--- src/app/(experiments)/2-css-version/starter/index.tsx
+++ src/app/(experiments)/2-css-version/starter/index.tsx
@@ -1,13 +1,56 @@
+import React from "react";
+
+const text = ["CSS".split(""), "version".split("")];
+
+const STAGGER = 0.3;
+const styles: React.CSSProperties = {
+  display: "inline-block",
+  animation: "reveal 0.4s cubic-bezier(0, 0.55, 0.45, 1)",
+  animationDelay: `calc( var( --delay , 0 ) * ${STAGGER}s)`,
+  animationFillMode: "both",
+};
+
+// you should use css modules or styled components here
+let customKeyframes = `
+	@keyframes reveal {
+		from {
+			opacity: 0;
+			transform: translateY(200px);
+		}
+		to {
+			opacity: 1;
+			transform: translateY(0px);
+		}
+	}
+`;
+
 export default function Page() {
   return (
-    <div className="bg-blue-300 text-black">
-      <div className="flex h-screen items-end justify-left overflow-hidden">
-        <h1 className="title font-black text-[min(20rem,30vw)] leading-none pb-[0.1em] text-left">
-          CSS
-          <br />
-          version
-        </h1>
+    <>
+      <style>{customKeyframes}</style>
+      <div className="bg-blue-300 text-black">
+        <div className="flex h-screen items-end justify-left overflow-hidden">
+          <h1 className="title font-black text-[min(20rem,30vw)] leading-none pb-[0.1em] text-left">
+            {text[0].map((l, i) => (
+              <span
+                style={{ "--delay": i, ...styles } as React.CSSProperties}
+                key={i}
+              >
+                {l}
+              </span>
+            ))}
+            <br />
+            {text[1].map((l, i) => (
+              <span
+                style={{ "--delay": i, ...styles } as React.CSSProperties}
+                key={i}
+              >
+                {l}
+              </span>
+            ))}
+          </h1>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
